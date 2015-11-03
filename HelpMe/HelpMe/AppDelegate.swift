0824eda4c847ee394ee0e1dd6665a4a0ef92cbed
sahsubodh <sahsubodh@gmail.com>
2015-11-03 14:15:45 -0800

    Remove ParseTwitter

--- HelpMe/HelpMe/AppDelegate.swift
+++ HelpMe/HelpMe/AppDelegate.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import Parse
-import ParseTwitterUtils
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- HelpMe/HelpMe/UserViewController.swift
+++ HelpMe/HelpMe/UserViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import Parse
-import ParseTwitterUtils
-
 
 class UserViewController: UIViewController {
   
